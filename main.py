0be146f1f176db2d4a4d4a2a808bc7318bee3372
Dhrupadarun <dhrupadarun46@gmail.com>
2025-02-22 15:14:58 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+def on_on_chat():pass
+player.on_chat("Chicken", on_on_chat)
+mobs.spawn(CHICKEN, pos(0, 100, 0))
---
+++ main.ts
@@ -0,0 +1,4 @@
+player.onChat("Chicken", function on_on_chat() {
+    
+})
+mobs.spawn(CHICKEN, pos(0, 100, 0))
